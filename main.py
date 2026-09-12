445e62d087f39465a1b6588ce8a8e0aa7935aaed
aipiao <450752262@qq.com>
2026-09-12 16:31:22 +0800

    初次提交

--- main.py
+++ main.py
@@ -1,24 +1,96 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+import time
+from pathlib import Path
+import httpx
+import math
+from astrbot.api.event import filter, AstrMessageEvent
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core import AstrBotConfig
+import astrbot.api.message_components as Comp
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+plugin_dir = Path(__file__).parent
+error_img = str(plugin_dir / "resource" / "error_img.jpg")
+
+@register("nlrmeme", "aipiao_", "调用 NLR MEME/梗图插件", "1.0.0")
 class MyPlugin(Star):
-    def __init__(self, context: Context):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.config = config
+    cooldowns: dict[str, float] = {}
+
+    def cold_check(self,event: AstrMessageEvent,sender_id:str):
+        cold_time = self.config.time
+        last_time = self.cooldowns.get(sender_id)
+        now = time.time()
+
+        #管理员豁免，可在配置文件开关
+        if self.config.admin:
+            if event.is_admin():
+                return True,0
+
+        #首次调用放行
+        if last_time is None:
+            self.cooldowns[sender_id] = now
+            return True, 0
+
+        #冷却判断
+        elapsed = now - last_time
+        remain = cold_time - elapsed
+
+        if remain <= 0:
+            self.cooldowns[sender_id] = now
+            return True, 0
+
+        return False, remain
+
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        self._client = httpx.AsyncClient(timeout=float(self.config.timeout or 15))
+        logger.info('NLR MEME 加载成功！')
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("meme")
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        """随机获取一个meme"""
+        sender_id = event.get_sender_id()
+        try:
+            ok, remain = self.cold_check(event, sender_id)
+        except Exception as e:
+            logger.error(e)
+        else:
+            try:
+                if ok:
+                    response = await self._client.get(
+                        "https://moonlight.api.mtszedu.com/moonlight/meme/index"
+                    )
+                    response.raise_for_status()
+                    json = response.json()
+                    data_type = json["data"]["type"]
+                    if data_type == 'image':
+                        img = json["data"]["content"]
+                        yield event.image_result(img)
+                    elif data_type == 'text':
+                        text = json["data"]["text"]
+                        yield event.plain_result(text)
+                    else:
+                        yield event.plain_result('未知错误！请查看日志！')
+                        raise ValueError("未知错误")
+                else:
+                    yield event.plain_result(f'冷却中！请等待 {math.ceil(remain)} 秒')
+            except ValueError as e:
+                logger.error(e)
+            except Exception as e:
+                logger.error(e)
+                chain = [
+                    Comp.Plain('获取失败'),
+                    Comp.Image.fromFileSystem(error_img)
+                ]
+                yield event.chain_result(chain)
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        if getattr(self, "_client", None):
+            await self._client.aclose()
+        self.cooldowns.clear()
+        logger.info('NLR MEME 已卸载！')
